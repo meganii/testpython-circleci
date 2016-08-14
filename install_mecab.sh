087e07f296f19ac0058762014333b533110aed9b
meganii <yuhei24@gmail.com>
2016-08-14 17:45:45 +0900

    add mecab-config path

--- install_mecab.sh
+++ install_mecab.sh
@@ -31,7 +31,7 @@ cd /var/tmp
 curl -O https://mecab.googlecode.com/files/mecab-ipadic-2.7.0-20070801.tar.gz
 tar zxfv mecab-ipadic-2.7.0-20070801.tar.gz
 cd mecab-ipadic-2.7.0-20070801
-./configure --with-charset=utf8
+./configure --with-charset=utf8 --with-mecab-config=/usr/local/bin/mecab-config
 make
 make DESTDIR=$HOME install
 
